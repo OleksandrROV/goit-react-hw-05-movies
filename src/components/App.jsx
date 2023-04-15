1611e35074425c06d018b43385fd42fdeb1743f5
Oleksandr Rusnak <san.ru.k.ar@gmail.com>
2023-04-15 10:00:50 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './Navigation/Navigation';
+
+const Home = lazy(() => import('../pages/Home'));
+const Movie = lazy(() => import('../pages/Movie'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Navigation />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movies" element={<Movie />} />
+          <Route path="movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
-};
+}
+
+export default App;
